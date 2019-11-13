de44d31ca6895b4cac9e6575b4ed46369f86aad6
wybosys <wybosys@gmail.com>
2019-11-13 19:22:57 +0800

    prj

--- 3rd/make/sqlite.py
+++ 3rd/make/sqlite.py
@@ -5,13 +5,11 @@ import env, os
 class Sqlite(env.Library):
     def build(self):
         env.Library.build(self)
-        os.chdir('sqlite')
-        env.cmd("make clean")
+        os.chdir('sqlite')        
         env.cmd(
             env.CmdStr("./configure --prefix=$PREFIX --disable-static-shell --disable-dependency-tracking --enable-json1 --enable-fts5 --disable-fts4 --disable-fts3")
             .test(env.SHARED, "--enable-shared --disable-static", "--disable-shared --enable-static"))
-        env.cmd("make install")
-        env.cmd("make clean")
+        env.cmd("make install")        
     def clean(self):
         os.chdir('sqlite')        
         env.cmd("make clean")
